2bd432040b79166b6a0a3d552bc37998f0e89b65
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-11-12 11:49:40 -0200

    migrate angular service to factory

---
+++ src/main/webapp/shared/components/input/utils/factory.input.focus.js
@@ -0,0 +1,95 @@
+angular.module('input.utils').factory(
+
+  // factory name
+  'InputFocusFactory', 
+
+// dependencies injection
+['$timeout', 
+
+// factory definition
+function($timeout) {
+
+  var InputFocus = (function() {
+
+    // private
+    var $scope, 
+        $timeout, 
+        focusFieldNameArray
+        lastFocusInput = null,
+        toSelect = null;
+
+    function resetFocusFields() {
+      for(var i=0, len=focusFieldNameArray.length; i<len; i++) {
+        $scope[focusFieldNameArray[i]] = false;
+      }
+    };
+
+    function selectFocusField() {
+      if(lastFocusInput !== toSelect) {
+        resetFocusFields();
+
+        if(toSelect) {
+          $scope[toSelect] = true;
+          lastFocusInput = toSelect;
+        } 
+      }
+    }
+
+    //--- === ---
+
+    // class constructr
+    var InputFocus = function(timeout, paginationFor) {
+      $timeout = timeout;
+      if(paginationFor) this.classInfo = 'InputFocus for: ' + paginationFor;
+    };
+    var ClassDef = InputFocus; 
+    //---
+
+    // public
+    
+    ClassDef.prototype.config = function(scope, _focusFieldNameArray) {
+      $scope = scope;
+      focusFieldNameArray = _focusFieldNameArray; 
+      resetFocusFields();
+    };
+
+    ClassDef.prototype.setFocus = function(focusFieldName, wait) {
+      wait = wait || 100; // ms
+      toSelect = focusFieldName;
+      $timeout(selectFocusField, wait);
+    };
+
+    ClassDef.prototype.focusReset = function() {
+      lastFocusInput = null;
+      toSelect = null;
+      resetFocusFields();
+    };
+
+    //---
+
+    // return class definiton
+    return ClassDef;
+
+  })();
+
+  //---
+
+  var instanceCache = {};
+  function getInstance(name) {
+    var instance = instanceCache[name];
+    if(instance) {
+      return instance;
+    } else {
+      instance = new InputFocus($timeout, name);
+      instanceCache[name] = instance;
+      return instance;
+    }
+  };
+
+  return {
+    get: function(name) {
+      return getInstance(name);
+    }
+  };
+
+}]);
